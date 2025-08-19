e5496b22fc2c2a6f8996e8e52702af5cb4387c7b
DeepeshChauhan04 <deepeshc30@gmail.com>
2025-08-19 17:24:50 +0530

    account service inner file chang

--- src/main/java/com/sharemarket/invest/service/implementation/AccountServiceImpl.java
+++ src/main/java/com/sharemarket/invest/service/implementation/AccountServiceImpl.java
@@ -64,9 +64,7 @@ public class AccountServiceImpl implements AccountService {
             throw new ResponseStatusException(HttpStatus.NOT_FOUND, "User not found with ID");
         }
 
-        AccountResponse list = modelMapper.map(accountDetails, AccountResponse.class);
-        log.info("Account by id  {}", list.getAccountId());
-        return list;
+        return modelMapper.map(accountDetails, AccountResponse.class);
     }
 
     @Override
